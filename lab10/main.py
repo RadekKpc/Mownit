37e84c3e563fcf72d18f7863d4eab8bccfa9e2c6
RadekKpc <radek4ec@gmail.com>
2020-06-01 14:56:17 +0200

    zad1 b

--- lab10/main.py
+++ lab10/main.py
@@ -17,9 +17,23 @@ def my_dft(A):
     N =  len(A)
     return np.dot(fourier_matrix(N),A)
 
-print(my_dft([1,2,3,4]))
 
-print(dft(4))
-print(np.fft.fft([1,2,3,4]))
+# print(my_dft([1,2,3,4]))
+
+# print(dft(4))
+# print(np.fft.fft([1,2,3,4]))
 # print(np.ifft())
 # 2
+
+def my_idft(A):
+    N =  len(A)
+    F = np.conjugate(fourier_matrix(N))
+    F /= N
+    return np.dot(F,A)
+
+# print(np.fft.ifft([1,2,3,4]))
+# print(my_idft([1,2,3,4]))
+
+
+# 3
+
